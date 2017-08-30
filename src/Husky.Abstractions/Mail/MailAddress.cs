2c61ca52c110e566f300a9841825881e240f30f6
Weixing Chen <chenwx@outlook.com>
2017-08-30 23:03:18 +0800

    staging. add Husky.PrincipalBaggage

--- src/Husky.Abstractions/Mail/MailAddress.cs
+++ src/Husky.Abstractions/Mail/MailAddress.cs
@@ -8,32 +8,32 @@ namespace Husky.Mail
 		public string Name { get; set; }
 		public string Address { get; set; }
 
-		public static MailAddress Parse(string formattedString) {
-			if ( formattedString == null ) {
-				throw new ArgumentNullException(nameof(formattedString));
+		public static MailAddress Parse(string mailAddressString) {
+			if ( mailAddressString == null ) {
+				throw new ArgumentNullException(nameof(mailAddressString));
 			}
-			if ( formattedString.IsEmail() ) {
-				return new MailAddress { Address = formattedString };
+			if ( mailAddressString.IsEmail() ) {
+				return new MailAddress { Address = mailAddressString };
 			}
-			if ( formattedString.IndexOf('<') != -1 && formattedString.EndsWith(">") ) {
-				var name = formattedString.Left("<", true).Trim();
-				var address = formattedString.Right("<", true).TrimEnd('>');
+			if ( mailAddressString.IndexOf('<') != -1 && mailAddressString.EndsWith(">") ) {
+				var name = mailAddressString.Left("<", true).Trim();
+				var address = mailAddressString.Right("<", true).TrimEnd('>');
 				if ( address.IsEmail() ) {
 					return new MailAddress { Name = name, Address = address };
 				}
 			}
-			throw new FormatException($"'{formattedString}' is an invalid mail box address format. Valid format should be like 'Your Name<youraccount@domain.com>'.");
+			throw new FormatException($"'{mailAddressString}' is an invalid mail box address format. Valid format should be like 'Your Name<youraccount@domain.com>'.");
 		}
 
-		public static bool TryParse(string formattedString, out MailAddress mailAddress) {
-			if ( formattedString != null ) {
-				if ( formattedString.IsEmail() ) {
-					mailAddress = new MailAddress { Address = formattedString };
+		public static bool TryParse(string mailAddressString, out MailAddress mailAddress) {
+			if ( mailAddressString != null ) {
+				if ( mailAddressString.IsEmail() ) {
+					mailAddress = new MailAddress { Address = mailAddressString };
 					return true;
 				}
-				if ( formattedString.IndexOf('<') != -1 && formattedString.EndsWith(">") ) {
-					var name = formattedString.Left("<", true);
-					var address = formattedString.Right("<", true).TrimEnd('>');
+				if ( mailAddressString.IndexOf('<') != -1 && mailAddressString.EndsWith(">") ) {
+					var name = mailAddressString.Left("<", true);
+					var address = mailAddressString.Right("<", true).TrimEnd('>');
 					if ( address.IsEmail() ) {
 						mailAddress = new MailAddress { Name = name, Address = address };
 						return true;
