1a4976f69dff05c0d20034ee4492b8245a771dcb
Weixing Chen <chenwx@outlook.com>
2017-09-29 12:25:50 +0800

    stage.

---
+++ usages/Insider.Portal/Pages/Logout.cshtml.cs
@@ -0,0 +1,19 @@
+using Husky.Authentication.Abstractions;
+using Husky.Injection;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Insider.Portal.Pages
+{
+	public class LogoutModel : PageModel
+	{
+		public LogoutModel(IPrincipal principal) {
+			_my = principal;
+		}
+
+		readonly IPrincipal _my;
+
+		public void OnGet() {
+			_my.User().SignOut();
+		}
+	}
+}
