77a8bb5a1a8e34f5b910f1bb13346ab3c7e94a19
Weixing Chen <chenwx@outlook.com>
2018-05-24 05:34:23 +0800

    add new property: Result.Code

--- src/Husky.Helpers/Models/Result.cs
+++ src/Husky.Helpers/Models/Result.cs
@@ -8,12 +8,14 @@ namespace Husky
 
 	public class Result : IActionResult
 	{
-		public Result(bool ok = false, string message = null) {
+		public Result(bool ok = false, string message = null, int? code = null) {
 			Ok = ok;
 			Message = message;
+			Code = code;
 		}
 		public virtual bool Ok { get; set; }
 		public virtual string Message { get; set; }
+		public virtual int? Code { get; set; }
 
 		public string ToJson() {
 			return JsonConvert.SerializeObject(this);
@@ -26,7 +28,7 @@ namespace Husky
 
 	public class Result<T> : Result
 	{
-		public Result(bool ok = false, string message = null, T data = default(T)) : base(ok, message) { Data = data; }
+		public Result(bool ok = false, string message = null, int? code = null, T data = default(T)) : base(ok, message, code) { Data = data; }
 		public T Data { get; set; }
 	}
 
@@ -37,14 +39,14 @@ namespace Husky
 	public class Success : Result
 	{
 		public Success() : base(true) { }
-		public Success(string message) : base(true, message) { }
+		public Success(string message) : base(true, message, null) { }
 	}
 
 	public class Success<T> : Result<T>
 	{
 		public Success() : base(true) { }
-		public Success(T data) : base(true, null, data) { }
-		public Success(string message, T data) : base(true, message, data) { }
+		public Success(T data) : base(true, null, null, data) { }
+		public Success(string message, T data) : base(true, message, null, data) { }
 
 	}
 
@@ -55,13 +57,13 @@ namespace Husky
 	public class Failure : Result
 	{
 		public Failure(string message) : base(false, message) { }
-
-		public static Failure NoPermission => new Failure("You account does not have permission to take this action");
+		public Failure(string message, int code) : base(false, message, code) { }
 	}
 
 	public class Failure<T> : Result<T>
 	{
 		public Failure(string message) : base(false, message) { }
+		public Failure(string message, int code) : base(false, message, code) { }
 	}
 
 	#endregion
