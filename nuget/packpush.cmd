8676688d58d361892eded3a1a59fb85e9446a87b
Weixing Chen <chenwx@outlook.com>
2019-10-21 13:56:15 +0800

    minor

--- nuget/packpush.cmd
+++ nuget/packpush.cmd
@@ -1,5 +1,5 @@
-dotnet pack ../ -c Release -o ../../nuget
+dotnet pack ../ -c Release -o ../nuget
 pause
-nuget push *.nupkg [your key] -src https://www.nuget.org/
+nuget push *.nupkg [key] -src https://www.nuget.org/
 del *.nupkg /f /q
 pause
