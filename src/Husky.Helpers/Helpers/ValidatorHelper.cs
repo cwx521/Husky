bd2f8cc4ec6f04d54434b7e988a468c16a4456b3
Weixing Chen <chenwx@outlook.com>
2020-10-15 12:57:39 +0800

    add ValidatorHelper

---
+++ src/Husky.Helpers/Helpers/ValidatorHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Husky
+{
+	public static class ValidatorHelper
+	{
+		public static Result Validate<T>(T instance) {
+			var validationResults = new List<ValidationResult>();
+			Validator.TryValidateObject(instance, new ValidationContext(instance), validationResults);
+
+			if ( validationResults.Count == 0 ) {
+				return new Success();
+			}
+			return new Failure(validationResults.First().ErrorMessage);
+		}
+	}
+}
