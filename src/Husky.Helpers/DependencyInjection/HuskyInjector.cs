23eaeb82b05b83c10c18d6daee71d01ade5d104c
Weixing Chen <chenwx@outlook.com>
2020-10-28 22:18:25 +0800

    refactoring

--- src/Husky.Helpers/DependencyInjection/HuskyInjector.cs
+++ src/Husky.Helpers/DependencyInjection/HuskyInjector.cs
@@ -20,102 +20,102 @@ namespace Husky
 		public IServiceCollection Services { get; }
 
 
-		public HuskyInjector MapKeyValueManager<TImplement>() where TImplement : class, IKeyValueManager {
+		public HuskyInjector AddExternalKeyValueManager<TImplement>() where TImplement : class, IKeyValueManager {
 			Services.AddScoped<IKeyValueManager, TImplement>();
 			return this;
 		}
-		public HuskyInjector AddKeyValueManager<TImplement>(Func<IServiceProvider, TImplement> implementationFactory)
+		public HuskyInjector AddExternalKeyValueManager<TImplement>(Func<IServiceProvider, TImplement> implementationFactory)
 			where TImplement : class, IKeyValueManager {
 			Services.AddScoped<IKeyValueManager, TImplement>(implementationFactory);
 			return this;
 		}
 
 
-		public HuskyInjector MapDiagnosticsLogger<TImplement>() where TImplement : class, IDiagnosticsLogger {
+		public HuskyInjector AddExternalDiagnosticsLogger<TImplement>() where TImplement : class, IDiagnosticsLogger {
 			Services.AddScoped<IDiagnosticsLogger, TImplement>();
 			return this;
 		}
-		public HuskyInjector AddDiagnosticsLogger<TImplement>(Func<IServiceProvider, TImplement> implementationFactory)
+		public HuskyInjector AddExternalDiagnosticsLogger<TImplement>(Func<IServiceProvider, TImplement> implementationFactory)
 			where TImplement : class, IDiagnosticsLogger {
 			Services.AddScoped<IDiagnosticsLogger, TImplement>(implementationFactory);
 			return this;
 		}
 
 
-		public HuskyInjector MapLbs<TImplement>() where TImplement : class, ILbs {
+		public HuskyInjector AddExternalLbs<TImplement>() where TImplement : class, ILbs {
 			Services.AddScoped<ILbs, TImplement>();
 			return this;
 		}
-		public HuskyInjector AddLbs<TImplement>(Func<IServiceProvider, TImplement> implementationFactory)
+		public HuskyInjector AddExternalLbs<TImplement>(Func<IServiceProvider, TImplement> implementationFactory)
 			where TImplement : class, ILbs {
 			Services.AddScoped<ILbs, TImplement>(implementationFactory);
 			return this;
 		}
 
 
-		public HuskyInjector MapMailSender<TImplement>() where TImplement : class, IMailSender {
+		public HuskyInjector AddExternalMailSender<TImplement>() where TImplement : class, IMailSender {
 			Services.AddScoped<IMailSender, TImplement>();
 			return this;
 		}
-		public HuskyInjector AddMailSender<TImplement>(Func<IServiceProvider, TImplement> implementationFactory)
+		public HuskyInjector AddExternalMailSender<TImplement>(Func<IServiceProvider, TImplement> implementationFactory)
 			where TImplement : class, IMailSender {
 			Services.AddScoped<IMailSender, TImplement>(implementationFactory);
 			return this;
 		}
 
 
-		public HuskyInjector MapSmsSender<TImplement>() where TImplement : class, ISmsSender {
+		public HuskyInjector AddExternalSmsSender<TImplement>() where TImplement : class, ISmsSender {
 			Services.AddScoped<ISmsSender, TImplement>();
 			return this;
 		}
-		public HuskyInjector AddSmsSender<TImplement>(Func<IServiceProvider, TImplement> implementationFactory)
+		public HuskyInjector AddExternalSmsSender<TImplement>(Func<IServiceProvider, TImplement> implementationFactory)
 			where TImplement : class, ISmsSender {
 			Services.AddScoped<ISmsSender, TImplement>(implementationFactory);
 			return this;
 		}
 
 
-		public HuskyInjector MapTwoFactor<TImplement>() where TImplement : class, ITwoFactorManager {
+		public HuskyInjector AddExternalTwoFactor<TImplement>() where TImplement : class, ITwoFactorManager {
 			Services.AddScoped<ITwoFactorManager, TImplement>();
 			return this;
 		}
-		public HuskyInjector AddTwoFactor<TImplement>(Func<IServiceProvider, TImplement> implementationFactory)
+		public HuskyInjector AddExternalTwoFactor<TImplement>(Func<IServiceProvider, TImplement> implementationFactory)
 			where TImplement : class, ITwoFactorManager {
 			Services.AddScoped<ITwoFactorManager, TImplement>(implementationFactory);
 			return this;
 		}
 
 
-		public HuskyInjector MapIdentityManager<TImplement>()
+		public HuskyInjector AddExternalIdentityManager<TImplement>()
 			where TImplement : class, IIdentityManager {
 			Services.AddScoped<IIdentityManager, TImplement>();
 			return this;
 		}
-		public HuskyInjector AddIdentityManager<TImplement>(Func<IServiceProvider, TImplement> implementationFactory)
+		public HuskyInjector AddExternalIdentityManager<TImplement>(Func<IServiceProvider, TImplement> implementationFactory)
 			where TImplement : class, IIdentityManager {
 			Services.AddScoped<IIdentityManager, TImplement>(implementationFactory);
 			return this;
 		}
 
 
-		public HuskyInjector MapPrincipal<TImplement>()
+		public HuskyInjector AddExternalPrincipal<TImplement>()
 			where TImplement : class, IPrincipalUser {
 			Services.AddScoped<IPrincipalUser, TImplement>();
 			return this;
 		}
-		public HuskyInjector AddPrincipal<TImplement>(Func<IServiceProvider, TImplement> implementationFactory)
+		public HuskyInjector AddExternalPrincipal<TImplement>(Func<IServiceProvider, TImplement> implementationFactory)
 			where TImplement : class, IPrincipalUser {
 			Services.AddScoped<IPrincipalUser, TImplement>(implementationFactory);
 			return this;
 		}
 
 
-		public HuskyInjector MapPrincipalAdmin<TImplement>()
+		public HuskyInjector AddExternalPrincipalAdmin<TImplement>()
 			where TImplement : class, IPrincipalAdmin {
 			Services.AddScoped<IPrincipalAdmin, TImplement>();
 			return this;
 		}
-		public HuskyInjector AddPrincipalAdmin<TImplement>(Func<IServiceProvider, TImplement> implementationFactory)
+		public HuskyInjector AddExternalPrincipalAdmin<TImplement>(Func<IServiceProvider, TImplement> implementationFactory)
 			where TImplement : class, IPrincipalAdmin {
 			Services.AddScoped<IPrincipalAdmin, TImplement>(implementationFactory);
 			return this;
