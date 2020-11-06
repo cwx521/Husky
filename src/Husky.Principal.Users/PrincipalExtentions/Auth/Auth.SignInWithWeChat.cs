d2e2913badae0f7389a4bd23d8213fd00936b3aa
Weixing Chen <chenwx@outlook.com>
2020-11-06 15:39:28 +0800

    refactoring

--- src/Husky.Principal.Users/PrincipalExtentions/Auth/Auth.SignInWithWeChat.cs
+++ src/Husky.Principal.Users/PrincipalExtentions/Auth/Auth.SignInWithWeChat.cs
@@ -23,68 +23,77 @@ namespace Husky.Principal.Users
 				return new Failure(LoginResult.FailureWeChatRequestToken.ToLabel());
 			}
 
-			var wechatUser = await wechat.GetUserInfoAsync(accessToken);
-			if ( !wechatUser.Ok ) {
-				return new Failure(LoginResult.FailureWeChatRequestUserInfo.ToLabel());
-			}
-
-			//寻找用户，看该微信账号是否已经注册过
 			var user = _db.Users
-				.Include(x => x.WeChat)
-				.Where(x => x.WeChat != null)
-				.Where(x => x.WeChat!.UnionId == wechatUser.UnionId || x.WeChat.OpenIds.Any(y => y.OpenIdValue == wechatUser.OpenId))
+				.AsNoTracking()
+				.Where(x => x.WeChat != null && x.WeChat.OpenIds.Any(y => y.OpenIdValue == accessToken.OpenId))
 				.SingleOrDefault();
 
-			//如果通过传入微信信息没找到已注册用户，判断用户当前是否已经通过其它方式登录，是的话直接使用该用户身份
-			if ( user == null && _me.IsAuthenticated ) {
-				user = _db.Users
-					.Where(x => x.Id == _me.Id)
-					.SingleOrDefault();
-			}
-
-			//如果都没有，就新建用户
+			//用户还不存在，即该微信账号第一次登录，进一步读取用户资料完成自动注册
 			if ( user == null ) {
-				user = new User();
-				_db.Users.Add(user);
-			}
-			else {
-				//用户记录是异常状态时，阻止获得登录身份
-				if ( user.Status == RowStatus.Suspended ) {
-					return await AddLoginRecordAsync(LoginResult.RejectedAccountSuspended, "WeChatApi", user.Id);
+
+				var wechatUser = await wechat.GetUserInfoAsync(accessToken);
+				if ( !wechatUser.Ok ) {
+					return new Failure(LoginResult.FailureWeChatRequestUserInfo.ToLabel());
 				}
-				if ( user.Status == RowStatus.Deleted ) {
-					return await AddLoginRecordAsync(LoginResult.RejectedAccountDeleted, "WeChatApi", user.Id);
+
+				//寻找用户，看该微信账号是否已经注册过
+				user = _db.Users
+					.Include(x => x.WeChat)
+					.Where(x => x.WeChat != null)
+					.Where(x => x.WeChat!.UnionId == wechatUser.UnionId || x.WeChat.OpenIds.Any(y => y.OpenIdValue == wechatUser.OpenId))
+					.SingleOrDefault();
+
+				//如果通过传入微信信息没找到已注册用户，判断用户当前是否已经通过其它方式登录，是的话直接使用该用户身份
+				if ( user == null && _me.IsAuthenticated ) {
+					user = _db.Users
+						.Where(x => x.Id == _me.Id)
+						.SingleOrDefault();
 				}
-				if ( user.Status != RowStatus.Active ) {
-					return await AddLoginRecordAsync(LoginResult.RejectedAccountInactive, "WeChatApi", user.Id);
+
+				//如果都没有，就新建用户
+				if ( user == null ) {
+					user = new User();
+					_db.Users.Add(user);
 				}
+				else {
+					//用户记录是异常状态时，阻止获得登录身份
+					if ( user.Status == RowStatus.Suspended ) {
+						return await AddLoginRecordAsync(LoginResult.RejectedAccountSuspended, "WeChatApi", user.Id);
+					}
+					if ( user.Status == RowStatus.Deleted ) {
+						return await AddLoginRecordAsync(LoginResult.RejectedAccountDeleted, "WeChatApi", user.Id);
+					}
+					if ( user.Status != RowStatus.Active ) {
+						return await AddLoginRecordAsync(LoginResult.RejectedAccountInactive, "WeChatApi", user.Id);
+					}
+				}
+
+				//更新 User 表字段
+				user.DisplayName ??= wechatUser.NickName.Left(36);
+				user.PhotoUrl ??= wechatUser.HeadImageUrl;
+
+				//更新 UserWeChat 表字段
+				user.WeChat ??= new UserWeChat();
+
+				if ( !user.WeChat.OpenIds.Any(x => x.OpenIdValue == wechatUser.OpenId) ) {
+					user.WeChat.OpenIds.Add(new UserWeChatOpenId {
+						OpenIdType = (WeChatField)(int)idSecret.Type,
+						OpenIdValue = wechatUser.OpenId
+					});
+				}
+				user.WeChat.UnionId = wechatUser.UnionId;
+				user.WeChat.NickName = wechatUser.NickName.Left(36)!;
+				user.WeChat.Sex = wechatUser.Sex;
+				user.WeChat.HeadImageUrl = wechatUser.HeadImageUrl;
+				user.WeChat.Province = wechatUser.Province?.Left(24);
+				user.WeChat.City = wechatUser.City?.Left(24);
+				user.WeChat.Country = wechatUser.Country?.Left(24);
+
+				await _db.Normalize().SaveChangesAsync();
 			}
-
-			//更新 User 表字段
-			user.DisplayName ??= wechatUser.NickName.Left(36);
-			user.PhotoUrl ??= wechatUser.HeadImageUrl;
-
-			//更新 UserWeChat 表字段
-			user.WeChat ??= new UserWeChat();
-
-			if ( !user.WeChat.OpenIds.Any(x => x.OpenIdValue == wechatUser.OpenId) ) {
-				user.WeChat.OpenIds.Add(new UserWeChatOpenId {
-					OpenIdType = (WeChatField)(int)idSecret.Type,
-					OpenIdValue = wechatUser.OpenId
-				});
-			}
-			user.WeChat.UnionId = wechatUser.UnionId;
-			user.WeChat.NickName = wechatUser.NickName.Left(36)!;
-			user.WeChat.Sex = wechatUser.Sex;
-			user.WeChat.HeadImageUrl = wechatUser.HeadImageUrl;
-			user.WeChat.Province = wechatUser.Province?.Left(24);
-			user.WeChat.City = wechatUser.City?.Left(24);
-			user.WeChat.Country = wechatUser.Country?.Left(24);
-
-			await _db.Normalize().SaveChangesAsync();
 
 			_me.Id = user.Id;
-			_me.DisplayName = user.DisplayName!;
+			_me.DisplayName = user.DisplayName ?? $"User#{user.Id}";
 			_me.IdentityManager?.SaveIdentity(_me);
 
 			return await AddLoginRecordAsync(LoginResult.Success, "WeChatApi", user.Id);
