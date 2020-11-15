5e45c914f76fcb9c1f3fe2d053f9dee5236db583
Weixing Chen <chenwx@outlook.com>
2020-11-15 14:38:26 +0800

    added new feature

--- src/Husky.FileStore/DependencyInjection.cs
+++ src/Husky.FileStore/DependencyInjection.cs
@@ -2,6 +2,7 @@
 using Husky.FileStore;
 using Husky.FileStore.AliyunOss;
 using Husky.FileStore.Data;
+using Husky.FileStore.LocalStorage;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -9,6 +10,11 @@ namespace Husky
 {
 	public static class DependencyInjection
 	{
+		public static HuskyInjector AddLocalFileBucket(this HuskyInjector husky, string fileBucketRootPath) {
+			husky.Services.AddSingleton<IFileBucket>(new LocalFileBucket(fileBucketRootPath));
+			return husky;
+		}
+
 		public static HuskyInjector AddAliyunOssBucket(this HuskyInjector husky, AliyunOssBucketOptions options) {
 			husky.Services.AddSingleton<ICloudFileBucket>(new AliyunOssBucket(options));
 			return husky;
---
+++ src/Husky.FileStore/LocalStorage/LocalFileBucket.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Husky.FileStore.LocalStorage
+{
+	public class LocalFileBucket : IFileBucket
+	{
+		public LocalFileBucket(string rootPath) {
+			ValidateRootPath(_rootPath = rootPath);
+		}
+
+		private string _rootPath;
+
+		public void OpenBucket(string bucketName) => ValidateRootPath(_rootPath = bucketName);
+
+		public Stream Get(string fileName) {
+			return new FileStream(GetPhysicalPath(fileName), FileMode.Open);
+		}
+
+		public void Put(string fileName, Stream data) {
+			var type = StoredFileTypeHelper.Identify(fileName);
+			if ( type == StoredFileType.Else ) {
+				throw new InvalidDataException($"Not allowed to upload {Path.GetExtension(fileName)} files or files without any extension name.");
+			}
+
+			var filePath = GetPhysicalPath(fileName);
+			var directory = Path.GetDirectoryName(filePath)!;
+			var bytes = new byte[data.Length];
+
+			data.Read(bytes);
+			Directory.CreateDirectory(directory);
+			File.WriteAllBytes(filePath, bytes);
+		}
+
+		public void Delete(string fileName) {
+			File.Delete(GetPhysicalPath(fileName));
+		}
+
+		public void Delete(string[] fileNames) {
+			fileNames.AsParallel().ForAll(fileName => Delete(fileName));
+		}
+
+		private string GetPhysicalPath(string fileName) => Path.Combine(_rootPath, fileName.TrimStart('/', '\\'));
+
+		private static void ValidateRootPath(string rootPath) {
+			if ( !Path.IsPathRooted(rootPath) ) {
+				throw new InvalidProgramException($"{rootPath} is not a root path");
+			}
+		}
+	}
+}
