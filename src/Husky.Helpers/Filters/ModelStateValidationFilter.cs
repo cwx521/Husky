4fcec071bcd46850164cfb17bbfc5444d88fc073
Weixing Chen <chenwx@outlook.com>
2023-07-23 17:31:13 +0800

    staging

---
+++ src/Husky.Helpers/Filters/ModelStateValidationFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Husky
+{
+	public class ModelStateValidationFilter : IPageFilter, IActionFilter
+	{
+		public void OnPageHandlerExecuting(PageHandlerExecutingContext context) {
+			if ( !context.ModelState.IsValid ) {
+				context.Result = context.ModelState.ToResult();
+			}
+		}
+
+		public void OnActionExecuting(ActionExecutingContext context) {
+			if ( !context.ModelState.IsValid ) {
+				context.Result = context.ModelState.ToResult();
+			}
+		}
+
+		public void OnPageHandlerSelected(PageHandlerSelectedContext context) { }
+		public void OnPageHandlerExecuted(PageHandlerExecutedContext context) { }
+		public void OnActionExecuted(ActionExecutedContext context) { }
+	}
+}
